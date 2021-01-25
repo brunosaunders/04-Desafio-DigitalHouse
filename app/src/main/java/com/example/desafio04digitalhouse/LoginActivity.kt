d122e7835e9e49f60f005159c348184143edf6b2
Bruno Saunders <senabruno97@hotmail.com>
2021-01-25 18:58:19 -0300

    authentication with email and password

--- app/src/main/java/com/example/desafio04digitalhouse/LoginActivity.kt
+++ app/src/main/java/com/example/desafio04digitalhouse/LoginActivity.kt
@@ -2,15 +2,30 @@ package com.example.desafio04digitalhouse
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         navigateToRegister()
+
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     private fun navigateToRegister() {
@@ -18,5 +33,33 @@ class LoginActivity : AppCompatActivity() {
         registerButton.setOnClickListener {
             startActivity(Intent(applicationContext, RegisterActivity::class.java))
         }
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+        if (currentUser != null)
+            startActivity(Intent(applicationContext, GameActivity::class.java))
+    }
+
+    fun extractFields(): List<String> {
+        val email = findViewById<EditText>(R.id.edt_email)
+        val password = findViewById<EditText>(R.id.edt_password)
+
+        return listOf(email.text.toString(), password.text.toString())
+    }
+
+    private fun signInWithEmailAndPassword() {
+        val fields = extractFields()
+        auth.signInWithEmailAndPassword(fields[0], fields[1])
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d("Login", "SignInWithEmail:Successful")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        Log.w("Login", "SignInWithEmail:Failure")
+                        Toast.makeText(applicationContext, "Authentication failed", Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
     }
 }
--- app/src/main/java/com/example/desafio04digitalhouse/RegisterActivity.kt
+++ app/src/main/java/com/example/desafio04digitalhouse/RegisterActivity.kt
@@ -1,12 +1,46 @@
 package com.example.desafio04digitalhouse
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        auth = FirebaseAuth.getInstance()
+        findViewById<Button>(R.id.btn_signUp).setOnClickListener {
+            signUp()
+        }
     }
+
+    private fun signUp() {
+        val email = findViewById<EditText>(R.id.edt_email)
+        val password = findViewById<EditText>(R.id.edt_password)
+        val repeatPassword = findViewById<EditText>(R.id.edt_repeat_password)
+
+        if (password.text != repeatPassword.text) {
+            Toast.makeText(applicationContext, "Passwords don't match", Toast.LENGTH_SHORT).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email.toString(), password.toString())
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("Register", "SignUpWithEmail:Successful")
+                            navigateToGame()
+                        } else {
+                            Log.w("Register", "SignUpWithEmail:Failure")
+                            Toast.makeText(applicationContext, "Sign Up failed", Toast.LENGTH_SHORT).show()
+                        }
+            }
+        }
+    }
+
+    private fun navigateToGame() = startActivity(Intent(applicationContext, GameActivity::class.java))
 }
