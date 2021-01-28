a2b5f807624e199292a5d40a1fec7415705888d4
Bruno Saunders <senabruno97@hotmail.com>
2021-01-28 18:57:24 -0300

    searchView

--- app/src/main/java/com/example/desafio04digitalhouse/GameActivity.kt
+++ app/src/main/java/com/example/desafio04digitalhouse/GameActivity.kt
@@ -1,20 +1,13 @@
 package com.example.desafio04digitalhouse
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
-        findViewById<Button>(R.id.btn_signOut).setOnClickListener {
-            Firebase.auth.signOut()
-            startActivity(Intent(applicationContext, LoginActivity::class.java))
-        }
+
     }
 }
--- app/src/main/java/com/example/desafio04digitalhouse/HomeFragment.kt
+++ app/src/main/java/com/example/desafio04digitalhouse/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
@@ -14,8 +17,32 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<SearchView>(R.id.searchView).apply {
+            setOnSearchClickListener {
+                hide(view)
+            }
+            setOnCloseListener {
+                show(view)
+                false
+            }
+        }
 
-        
         return view
+    }
+
+    private fun hide(view: View) {
+        val hint = view.findViewById<TextView>(R.id.tv_query_hint)
+        val micro = view.findViewById<ImageView>(R.id.icon_micro)
+
+        hint.visibility = View.GONE
+        micro.visibility = View.GONE
+    }
+
+    private fun show(view: View) {
+        val hint = view.findViewById<TextView>(R.id.tv_query_hint)
+        val micro = view.findViewById<ImageView>(R.id.icon_micro)
+
+        hint.visibility = View.VISIBLE
+        micro.visibility = View.VISIBLE
     }
 }
